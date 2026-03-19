abddcc80668f00b705b609fecf459d627940d537
guilherme de araujo <guiam2909@gmail.com>
2026-03-19 11:09:11 -0300

    trocando logica do menu principal

--- src/br/com/fiap/main/MainTv.java
+++ src/br/com/fiap/main/MainTv.java
@@ -8,39 +8,34 @@ public class MainTv {
     public static void main(String[] args) {
         Televisor tv = new Televisor();
         Scanner scan = new Scanner(System.in);
+        int volume, canal, opcao;
         try {
             System.out.print("Informe um canal: ");
-            int canalEscolhido = scan.nextInt();
+            canal = scan.nextInt();
             System.out.print("Informe o nivel do volume: ");
-            int volumeEscolhido = scan.nextInt();
-            tv.setCanal(canalEscolhido);
-            tv.setVolume(volumeEscolhido);
+            volume = scan.nextInt();
+            tv.setCanal(canal);
+            tv.setVolume(volume);
 
             System.out.println("1-Trocar Canal: \n2-Trocar Volume: \n3-Aumentar: \n4-Diminuir: ");
-            int opcao = scan.nextInt();
-            switch(opcao) {
-                case 1:
-                    System.out.print("Digite o novo canal: ");
-                    int novoCanal = scan.nextInt();
-                    tv.setCanal(novoCanal);
-                    break;
-                case 2:
-                    System.out.print("Digite o novo nivel de Volume: ");
-                    int novoVolume = scan.nextInt();
-                    tv.setVolume(novoVolume);
-                    break;
-                case 3:
-                    tv.aumentarVolume();
-                    System.out.print("Volume Aumentado");
-                    break;
-                case 4:
-                    tv.diminuirVolume();
-                    System.out.print("Volume diminuido.");
-                    break;
-                default:
-                    System.out.println("Opção inválida!");
+            opcao = scan.nextInt();
+            if (opcao == 1) {
+                System.out.print("Digite o novo canal: ");
+                canal = scan.nextInt();
+                tv.setCanal(canal);
+            } else if (opcao == 2) {
+                System.out.print("Digite o novo nivel de Volume: ");
+                volume = scan.nextInt();
+                tv.setVolume(volume);
+            } else if (opcao == 3) {
+                tv.aumentarVolume();
+                System.out.print("Volume Aumentado");
+            } else if (opcao == 4) {
+                tv.diminuirVolume();
+                System.out.print("Volume diminuido.");
+            } else {
+                System.out.println("Opção inválida!");
             }
-
             System.out.printf("Canal atual: %d %nVolume atual: %d%n", tv.getCanal(), tv.getVolume());
         } catch (Exception e) {
             System.out.println("Formato Invalido!");
